e6a7dfb58a1087fe950b4c2eb1d6361caaa6cfbc
Fomina Galyna <fominagv@gmail.com>
2016-02-29 23:41:50 -0800

    Add HW_DataTypes

---
+++ src/Lesson_01/HomeWork/HW_DataTypes.java
@@ -0,0 +1,34 @@
+package Lesson_01.HomeWork;
+
+public class HW_DataTypes {
+
+    public static void main(String[] args) {
+    // TODO Раскоментируйте и исправте ошибки компиляции
+        byte b=1;
+        short s=2;
+        int i=3;
+        long l=4;
+       // byte b1 = b + s;
+       // byte b2 = b + 1;
+       // byte b3 = b + 3l;
+       // byte b4 = b + l;
+       // short s1 = -s;
+       // short s1 = +s;
+       // int i1 = b + s + i + l;
+       // int i2 = 3*1l;
+
+        // TODO Что будет напечатано?
+         short x = -32768;
+        System.out.println("-x = " + (-x));
+        System.out.println("--x = " + (--x));
+
+        short y = 32767;
+        System.out.println("++y = " + (++y));
+        System.out.println("y+1 = " + (y+1));
+
+
+    // TODO Приведите пример когда вырожение -x==x истинно, используя переполнение типов (x!=0)
+
+
+    }
+}
